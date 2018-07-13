b539b443e9e117818d171beb06577181237001d3
Shannan Galbraith <s.galbraith08@gmail.com>
2018-07-14 04:39:33 +1000

    started vuejs, added toolbar and footer

---
+++ resources/assets/js/components/AppFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-footer absolute="true" class="pa-3">
+      Forum
+    <v-spacer></v-spacer>
+    <div>&copy; {{ new Date().getFullYear() }}</div>
+  </v-footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
