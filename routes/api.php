e0ca2b6dd4f3a9fe2905d5e6a6aa810c34df8bf7
Shannan Galbraith <s.galbraith08@gmail.com>
2018-07-14 00:51:44 +1000

    added jwt auth

--- routes/api.php
+++ routes/api.php
@@ -24,3 +24,18 @@ Route::apiResource('/question/{question}/reply', 'ReplyController');
 // likes
 Route::post('/like/{reply}', 'LikeController@likeIt')->name('likeit');
 Route::delete('/like/{reply}', 'LikeController@unLikeIt')->name('unlikeit');
+
+// Auth
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
